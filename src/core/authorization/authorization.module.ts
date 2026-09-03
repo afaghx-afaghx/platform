b41646603159bc509a189e743843f9260ed8faa4
AFAGHX/afaghx-platform <afaghxb@gmail.com>
2026-09-04 03:24:50 +0330

    security(authz): connect authorization boundary to audit foundation

--- src/core/authorization/authorization.module.ts
+++ src/core/authorization/authorization.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthenticationModule } from '../authentication/authentication.module';
+import { AuditModule } from '../audit/audit.module';
 import { AuthorizationService } from './authorization.service';
 import { AuthorizationGuard } from './authorization.guard';
 
 @Module({
-  imports: [PrismaModule, AuthenticationModule],
+  imports: [PrismaModule, AuthenticationModule, AuditModule],
   providers: [AuthorizationService, AuthorizationGuard],
   exports: [AuthorizationService, AuthorizationGuard],
 })
