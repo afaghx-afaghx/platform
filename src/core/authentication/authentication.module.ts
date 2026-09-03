f7b329d3460aeb7e5d5ff06efee7a830f5541319
AFAGHX/afaghx-platform <afaghxb@gmail.com>
2026-09-04 03:20:26 +0330

    feat(core): register security context guard

--- src/core/authentication/authentication.module.ts
+++ src/core/authentication/authentication.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthService } from './auth.service';
 import { PasswordService } from './password.service';
+import { SecurityContextGuard } from './security-context.guard';
 
 @Module({
   imports: [PrismaModule],
-  providers: [AuthService, PasswordService],
-  exports: [AuthService, PasswordService],
+  providers: [
+    AuthService,
+    PasswordService,
+    SecurityContextGuard,
+    { provide: APP_GUARD, useExisting: SecurityContextGuard },
+  ],
+  exports: [AuthService, PasswordService, SecurityContextGuard],
 })
 export class AuthenticationModule {}
