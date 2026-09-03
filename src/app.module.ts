b7e790c06992d544e5f0888b4098510bd660415d
AFAGHX/afaghx-platform <afaghxb@gmail.com>
2026-09-04 03:21:16 +0330

    feat(core): register audit foundation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { PrismaModule } from './core/prisma/prisma.module';
 import { AuthController } from './core/authentication/auth.controller';
 import { AuthenticationModule } from './core/authentication/authentication.module';
 import { AuthorizationModule } from './core/authorization/authorization.module';
+import { AuditModule } from './core/audit/audit.module';
 
 @Module({
-  imports: [PrismaModule, AuthenticationModule, AuthorizationModule],
+  imports: [PrismaModule, AuthenticationModule, AuthorizationModule, AuditModule],
   controllers: [AuthController],
 })
 export class AppModule {}
