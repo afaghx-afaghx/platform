dab9ccfb51b7eee3d1f8237f8518e5f6f5ec0a7d
AFAGHX/afaghx-platform <afaghxb@gmail.com>
2026-09-04 03:21:05 +0330

    feat(core): compose authorization guard boundary

--- src/core/authorization/authorization.module.ts
+++ src/core/authorization/authorization.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AuthenticationModule } from '../authentication/authentication.module';
 import { AuthorizationService } from './authorization.service';
+import { AuthorizationGuard } from './authorization.guard';
 
-@Module({ imports: [PrismaModule], providers: [AuthorizationService], exports: [AuthorizationService] })
+@Module({
+  imports: [PrismaModule, AuthenticationModule],
+  providers: [AuthorizationService, AuthorizationGuard],
+  exports: [AuthorizationService, AuthorizationGuard],
+})
 export class AuthorizationModule {}
