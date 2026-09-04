65fb44c6c15fb7b66bff4af44fe24c4fc5e85318
AFAGHX/afaghx-platform <afaghxb@gmail.com>
2026-09-04 04:01:18 +0330

    feat(core): register JWKS publication and MFA services

--- src/core/authentication/authentication.module.ts
+++ src/core/authentication/authentication.module.ts
@@ -6,16 +6,20 @@ import { AuthService } from './auth.service';
 import { PasswordService } from './password.service';
 import { SecurityContextGuard } from './security-context.guard';
 import { MfaService } from './mfa.service';
+import { JwksService } from './jwks.service';
+import { JwksController } from './jwks.controller';
 
 @Module({
   imports: [PrismaModule, AuditModule],
+  controllers: [JwksController],
   providers: [
     AuthService,
     PasswordService,
     MfaService,
+    JwksService,
     SecurityContextGuard,
     { provide: APP_GUARD, useExisting: SecurityContextGuard },
   ],
-  exports: [AuthService, PasswordService, MfaService, SecurityContextGuard],
+  exports: [AuthService, PasswordService, MfaService, JwksService, SecurityContextGuard],
 })
 export class AuthenticationModule {}
