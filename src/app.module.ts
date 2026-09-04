937ac6f9070eae3c001fbc43684fb5abb298ab0c
AFAGHX/afaghx-platform <afaghxb@gmail.com>
2026-09-04 09:54:50 +0330

    feat(core): integrate security module into application root

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AuthController } from './core/authentication/auth.controller';
 import { AuthenticationModule } from './core/authentication/authentication.module';
 import { AuthorizationModule } from './core/authorization/authorization.module';
 import { AuditModule } from './core/audit/audit.module';
+import { SecurityModule } from './core/security/security.module';
 
 @Module({
-  imports: [PrismaModule, AuthenticationModule, AuthorizationModule, AuditModule],
+  imports: [PrismaModule, AuthenticationModule, AuthorizationModule, AuditModule, SecurityModule],
   controllers: [AuthController],
 })
 export class AppModule {}
