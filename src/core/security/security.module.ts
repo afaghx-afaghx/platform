62f00385ae335d6b86728561394f4f8ebfe1ffec
AFAGHX/afaghx-platform <afaghxb@gmail.com>
2026-09-04 09:54:47 +0330

    feat(core): wire security services and global rate limiting

---
+++ src/core/security/security.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { MfaService } from './mfa.service';
+import { RecoveryService } from './recovery.service';
+import { RedisSessionStore } from './redis-session.store';
+import { KeyManager } from './key-manager';
+import { SecretBoxService } from './secret-box.service';
+
+@Module({
+  imports: [ThrottlerModule.forRoot([{ ttl: 60_000, limit: 120 }])],
+  providers: [
+    MfaService,
+    RecoveryService,
+    RedisSessionStore,
+    KeyManager,
+    SecretBoxService,
+    { provide: APP_GUARD, useClass: ThrottlerGuard },
+  ],
+  exports: [MfaService, RecoveryService, RedisSessionStore, KeyManager, SecretBoxService],
+})
+export class SecurityModule {}
