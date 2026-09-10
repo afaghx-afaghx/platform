0acd935f18bbd329bef2cc6d031b8f618ca3e43d
AFAGHX/afaghx-platform <afaghxb@gmail.com>
2026-09-10 15:45:23 +0330

    refactor(afx-core): export persistent WebAuthn components

--- core/AFX-CORE/src/index.js
+++ core/AFX-CORE/src/index.js
@@ -1,3 +1,7 @@
 export { AfxCore } from './core.js';
 export { hashPassword, verifyPassword, normalizeEmail, randomToken, tokenDigest, SECURITY_PARAMETERS } from './security.js';
 export { generateMfaSecret, generateRecoveryCodes, verifyTotp, verifyTotpStep, encryptMfaSecret, decryptMfaSecret, MFA_PARAMETERS } from './mfa.js';
+export { WebAuthnService, WEBAUTHN_PARAMETERS } from './webauthn.js';
+export { PersistentWebAuthnService } from './persistent-webauthn.js';
+export { PersistentAfxCore } from './persistent-core.js';
+export { AfxCoreRepository, PostgresAfxCoreRepository, AFX_CORE_SCHEMA } from './repository.js';
