9c2c6bb518b9b5cf93095a3907763afa893077e8
AFAGHX/afaghx-platform <afaghxb@gmail.com>
2026-09-20 18:04:37 +0330

    Revert unrelated Core change from Golden trigger PR: core/AFX-CORE/src/index.js

--- core/AFX-CORE/src/index.js
+++ core/AFX-CORE/src/index.js
@@ -1,4 +1,2 @@
 export { AfxCore } from './core.js';
-export { PersistentAfxCore } from './persistent-core.js';
-export { AfxCoreRepository, PostgresAfxCoreRepository, AFX_CORE_SCHEMA } from './repository.js';
 export { hashPassword, verifyPassword, normalizeEmail, randomToken, tokenDigest, SECURITY_PARAMETERS } from './security.js';
