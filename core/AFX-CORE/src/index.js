134b99e197e8509d966ebda979f843fa9737077f
AFAGHX/afaghx-platform <afaghxb@gmail.com>
2026-09-20 18:03:10 +0330

    Core: export persistent canonical runtime authority [AFX-GOLDEN-EXECUTE]

--- core/AFX-CORE/src/index.js
+++ core/AFX-CORE/src/index.js
@@ -1,2 +1,4 @@
 export { AfxCore } from './core.js';
+export { PersistentAfxCore } from './persistent-core.js';
+export { AfxCoreRepository, PostgresAfxCoreRepository, AFX_CORE_SCHEMA } from './repository.js';
 export { hashPassword, verifyPassword, normalizeEmail, randomToken, tokenDigest, SECURITY_PARAMETERS } from './security.js';
