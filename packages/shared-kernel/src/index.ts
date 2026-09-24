1433e0ad81bf2a9fe517e409b78fd908224a7ef5
AFAGHX/afaghx-platform <afaghxb@gmail.com>
2026-09-24 19:37:40 +0330

    feat(shared-kernel): establish canonical domain primitives

---
+++ packages/shared-kernel/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./result.js";
+export * from "./entity.js";
+export * from "./value-object.js";
+export * from "./domain-event.js";
+export * from "./clock.js";
+export * from "./id-generator.js";
